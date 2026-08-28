427a1e2bff00f502621260759f2a2271936dd6c4
boubaker-boulaid <boubakerboulaid205@gmail.com>
2026-08-28 09:46:18 +0100

    fix: rename histories method to history in Order and User models; update default values for status and payment_status in orders migration

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -32,7 +32,7 @@ class User extends Authenticatable
         ];
     }
 
-    public function histories()
+    public function history()
     {
         return $this->hasMany(OrderStatusHistory::class);
     }
